201f4df647c644e140930717a70b68ec53fe1f6c
MarcoChavezB <marco1102004@gmail.com>
2024-04-12 17:37:36 -0600

    test

--- test.py
+++ test.py
@@ -1,12 +1,31 @@
 from gpiozero import AngularServo
 from time import sleep
 
-servo = AngularServo(35, min_pulse_width=0.0006, max_pulse_width=0.0023)
+# Crea el servo motor con los pulsos mínimos y máximos ajustados para adaptarse a tu servo
+servo = AngularServo(35, min_angle=-90, max_angle=90, min_pulse_width=0.0006, max_pulse_width=0.0023)
 
-while (True):
-    servo.angle = 90
-    sleep(2)
+def move_to_90_degrees():
     servo.angle = 0
-    sleep(2)
-    servo.angle = -90
-    sleep(2)
+
+def move_to_120_degrees():
+    servo.angle = 30
+
+def move_to_20_degrees():
+    servo.angle = -70
+
+try:
+    while True:
+        # Espera la entrada del usuario
+        user_input = input("Ingrese 'o' para ir a 120 grados, 'p' para ir a 90 grados, 'q' para ir a 20 grados: ")
+        if user_input == 'o':
+            move_to_120_degrees()
+        elif user_input == 'p':
+            move_to_90_degrees()
+        elif user_input == 'q':
+            move_to_20_degrees()
+        else:
+            print("Entrada no válida.")
+except KeyboardInterrupt:
+    pass
+finally:
+    servo.detach()  # Libera los recursos del servo al finalizar
