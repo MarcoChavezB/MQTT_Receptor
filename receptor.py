97e1241218c279f8ec46e03c84cf85c192ea1817
MarcoChavezB <marco1102004@gmail.com>
2024-04-15 08:55:44 -0600

    cambios

--- receptor.py
+++ receptor.py
@@ -171,9 +171,9 @@ client.on_connect = on_connect
 client.on_message = on_message
 
 client.tls_set(
-    ca_certs='./AmazonRootCA1.pem',
-    certfile='./cec69141d6f3a0869a78f2331a3b6acebf6bc9ddb27a738dc3945c2ea4a99618-certificate.pem.crt',
-    keyfile='./cec69141d6f3a0869a78f2331a3b6acebf6bc9ddb27a738dc3945c2ea4a99618-private.pem.key',
+    ca_certs='/home/server/MQTT_Receptor/AmazonRootCA1.pem',
+    certfile='/home/server/MQTT_Receptor/cec69141d6f3a0869a78f2331a3b6acebf6bc9ddb27a738dc3945c2ea4a99618-certificate.pem.crt',
+    keyfile='/home/server/MQTT_Receptor/cec69141d6f3a0869a78f2331a3b6acebf6bc9ddb27a738dc3945c2ea4a99618-private.pem.key',
     tls_version=ssl.PROTOCOL_SSLv23)
 client.tls_insecure_set(True)
 
