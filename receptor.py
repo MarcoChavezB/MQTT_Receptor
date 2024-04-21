f2c7df6b7b2028b2b13112ee8f69f0967c470430
MarcoChavezB <marco1102004@gmail.com>
2024-04-21 16:48:04 -0600

    cambios en center camera

--- receptor.py
+++ receptor.py
@@ -41,7 +41,7 @@ def move_right_camera():
     camera_pwm.ChangeDutyCycle(0)
     
 def center_camera():
-    camera_pwm.ChangeDutyCycle(6)
+    camera_pwm.ChangeDutyCycle(6.2)
     sleep(0.5)
     camera_pwm.ChangeDutyCycle(0)
     
